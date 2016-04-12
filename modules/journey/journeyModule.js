4b6539bc293e752baeca68955eed3fa57b3f8b1d
calemany <calemany@student.42.fr>
2016-04-12 14:14:30 +0200

    Merge

--- modules/journey/journeyModule.js
+++ modules/journey/journeyModule.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 /**
 * @class angular_module.journey
 * @memberOf angular_module
 */
 
-angular.module('journey', ['data', 'tracking', 'utility'])
-=======
-angular.module('journey', ['ionic', 'data', 'tracking', 'utility']);
->>>>>>> 56ca46649ef48b2e18f2149e69bd019279470d45
+angular.module('journey', ['data', 'tracking', 'utility']);
